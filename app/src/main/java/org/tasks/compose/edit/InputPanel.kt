3b4b1455017171a6e84c7b49ce144bc31deb4dbb
hady <akhapugin@gmail.com>
2024-03-22 17:52:06 +0200

    Small tweeks of UI

--- app/src/main/java/org/tasks/compose/edit/InputPanel.kt
+++ app/src/main/java/org/tasks/compose/edit/InputPanel.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
@@ -48,7 +49,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntRect
 import androidx.compose.ui.unit.IntSize
@@ -160,7 +160,7 @@ private fun PopupContent(save: (String) -> Unit = {},
                 .fillMaxWidth()
                 .wrapContentHeight()
         ) {
-            val text = remember { mutableStateOf("") }
+            val text = rememberSaveable { mutableStateOf("") }
             val requester = remember { FocusRequester() }
 
             val doSave = {
