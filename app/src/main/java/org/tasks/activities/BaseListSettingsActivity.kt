31033b5a5eb0436e9f9cebd5e7c3b4d1a4da255f
hady <akhapugin@gmail.com>
2024-05-05 16:45:46 +0300

    solved the problem with initial value for defaultIcon

--- app/src/main/java/org/tasks/activities/BaseListSettingsActivity.kt
+++ app/src/main/java/org/tasks/activities/BaseListSettingsActivity.kt
@@ -47,10 +47,15 @@ abstract class BaseListSettingsActivity : ThemedInjectingAppCompatActivity(), Ic
     protected val textState = mutableStateOf("")
     protected val errorState = mutableStateOf("")
     protected val colorState = mutableStateOf(Color.Unspecified)
-    protected val iconState = mutableStateOf(R.drawable.ic_outline_label_24px)  //mutableStateOf(getIconResId(defaultIcon)!!)
+    protected val iconState = mutableStateOf(R.drawable.ic_outline_not_interested_24px)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /* defaultIcon is initialized in the descendant's constructor so it can not be used
+           in constructor of the base class. So valid initial value for iconState is set here  */
+        iconState.value = getIconResId(defaultIcon)!!
+
         if (!compose) {
             val view = bind()
             setContentView(view)
