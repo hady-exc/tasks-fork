f768f757b90a929cfa89b80efc4b3d1280c87888
hady <akhapugin@gmail.com>
2024-08-20 20:27:44 +0300

    Refactoring in GoogleTaskListSettingsActivity

--- app/src/main/java/org/tasks/activities/GoogleTaskListSettingsActivity.kt
+++ app/src/main/java/org/tasks/activities/GoogleTaskListSettingsActivity.kt
@@ -4,13 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.ProgressBar
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.material.SnackbarHostState
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.composethemeadapter.MdcTheme
-import com.google.android.material.textfield.TextInputEditText
 import com.google.api.services.tasks.model.TaskList
 import com.todoroo.astrid.activity.MainActivity
 import com.todoroo.astrid.activity.TaskListFragment
@@ -24,7 +22,6 @@ import org.tasks.R
 import org.tasks.Strings.isNullOrEmpty
 import org.tasks.compose.DeleteButton
 import org.tasks.compose.drawer.DrawerSnackBar
-import org.tasks.compose.drawer.ListSettingsDrawer
 import org.tasks.data.CaldavAccount
 import org.tasks.data.CaldavCalendar
 import org.tasks.data.GoogleTaskListDao
@@ -36,9 +33,6 @@ import javax.inject.Inject
 class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
     @Inject lateinit var googleTaskListDao: GoogleTaskListDao
     @Inject lateinit var taskDeleter: TaskDeleter
-
-    private lateinit var name: TextInputEditText
-    private lateinit var progressView: ProgressBar
 
     private var isNewList = false
     private lateinit var gtasksList: CaldavCalendar
@@ -66,16 +60,6 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
 
         if (!isNewList) textState.value = gtasksList.name!!
 
-        /*
-                if (isNewList) {
-                    name.requestFocus()
-                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.showSoftInput(name, InputMethodManager.SHOW_IMPLICIT)
-                } else {
-                    name.setText(gtasksList.name)
-                }
-        */
-
         if (createListViewModel.inProgress
                 || renameListViewModel.inProgress
                 || deleteListViewModel.inProgress) {
@@ -88,19 +72,10 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
 
         setContent {
             MdcTheme {
-                ListSettingsDrawer(
+                DefaultContent(
                     title = toolbarTitle,
                     requestKeyboard = isNewList,
-                    text = textState,
-                    error = errorState,
-                    color = colorState,
-                    icon = iconState,
-                    save = { lifecycleScope.launch { save() } },
-                    selectColor = { showThemePicker() },
-                    clearColor = { clearColor() },
-                    selectIcon = { showIconPicker() },
-                    optionButton = { if (!isNewList) DeleteButton { lifecycleScope.launch { promptDelete() } } },
-                    showProgress = showProgress
+                    optionButton = { if (!isNewList) DeleteButton{ promptDelete() } }
                 )
 
                 DrawerSnackBar(state = snackbar)
@@ -115,7 +90,7 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
         get() = isNewList
 
     override val toolbarTitle: String
-        get() = if (isNew) "NEW " + getString(R.string.new_list) else gtasksList.name!!
+        get() = if (isNew) getString(R.string.new_list) else gtasksList.name!!
 
     private fun showProgressIndicator() {
         showProgress.value = true
@@ -165,14 +140,6 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
     }
 
     override fun bind(): View { TODO() }
-
-/*
-    override fun bind() = ActivityGoogleTaskListSettingsBinding.inflate(layoutInflater).let {
-        name = it.name
-        progressView = it.progressBar.progressBar
-        it.root
-    }
-*/
 
     override fun promptDelete() {
         if (!requestInProgress()) {
