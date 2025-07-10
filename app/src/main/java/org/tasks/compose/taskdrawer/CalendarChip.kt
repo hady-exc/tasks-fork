f0e4a52e513e9e4840e315cc43462c7de944303d
hady <akhapugin@gmail.com>
2025-07-10 14:40:31 +0300

    incorrect title text in the CalendarChip fixed

--- app/src/main/java/org/tasks/compose/taskdrawer/CalendarChip.kt
+++ app/src/main/java/org/tasks/compose/taskdrawer/CalendarChip.kt
@@ -3,6 +3,10 @@ package org.tasks.compose.taskdrawer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Event
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import org.tasks.calendars.CalendarProvider
+import org.tasks.preferences.PermissionChecker
 
 private val calendarIcon = Icons.Outlined.Event
 
@@ -12,11 +16,18 @@ fun CalendarChip (
     select: () -> Unit,
     clear: () -> Unit
 ) {
-    if (selected.isNullOrBlank()) {
+    val context = LocalContext.current
+    val calendarProvider = remember {
+        CalendarProvider(context, PermissionChecker(context))
+    }
+    val title = selected?.let {
+        calendarProvider.getCalendar(it)?.name
+    }
+    if (title == null) {
         IconChip(icon = calendarIcon, action = select)
     } else {
         Chip(
-            title = selected,
+            title =  title,
             leading = calendarIcon,
             delete = clear,
             action = select
